5653b30fbbe13569df27f425ee9ca80578134ad0
muhmmadFaheem <www.faheem047@gmail.com>
2025-01-28 13:37:33 -0800

    second

---
+++ src/sentry.config.js
@@ -0,0 +1,11 @@
+const {withSentryConfig} = require("@sentry/nextjs");
+
+const moduleExports = {
+
+};
+
+const SentryWebpackPluginOptions = {
+    silent : true,
+};
+
+module.exports = withSentryConfig(moduleExports,SentryWebpackPluginOptions);
